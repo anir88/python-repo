17859107c0c34b27ebf05ba4fb34c561f8ff98bb
anir88 <anir.siliguri@gmail.com>
2024-05-09 22:25:55 +0530

    Adding a new program

---
+++ function-test.py
@@ -0,0 +1,19 @@
+#Python code to demonstrate the usage of functions
+
+print("Welcome to the City Descriptor!!!!")
+
+def main():
+    inp1 = input("Please enter the city name: ")
+    match inp1:
+        case "Guwahati":
+            print ("Also known as Pragjyotishpur")
+        case "Kolkata":
+            print("Also known as City of Joy")
+        case "Siliguri":
+            print("Also known as Gateway of Northeast")            
+        case _:
+            print("Unknown city")
+
+
+if __name__ == "__main__":
+    main()
